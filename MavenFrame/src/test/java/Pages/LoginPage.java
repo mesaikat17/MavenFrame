1def5821698b68892abf43f78a960789eab7c85b
mesaikat17 <mesaikat17@gmail.com>
2019-03-18 20:51:41 +0530

    Login Page Changes

--- MavenFrame/src/test/java/Pages/LoginPage.java
+++ MavenFrame/src/test/java/Pages/LoginPage.java
@@ -1,5 +1,39 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+	WebDriver driver;
+	
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	@FindBy (id="login-input") WebElement uname;
+	@FindBy (id="login-continue-btn") WebElement unameButton;
+	@FindBy (id="login-password") WebElement pass;
+	@FindBy (id="login-submit-btn") WebElement loginButton;
+	
+
+
+public void loginyatra(String username, String password) {
+	
+	uname.sendKeys(username);
+	unameButton.click();
+	
+	try {
+		Thread.sleep(3000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	pass.sendKeys(password);
+	loginButton.click();
+	
+}
 
 }
